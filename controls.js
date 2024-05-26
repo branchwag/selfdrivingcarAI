528c0e9dc01dae23c4e4fed3ec196167316d6ae3
LeAnne B. <branchleannelmb@gmail.com>
2024-05-26 11:42:45 -0400

    starts adding controls

---
+++ controls.js
@@ -0,0 +1,22 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyboardListeners();
+  }
+
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+      }
+    };
+  }
+}

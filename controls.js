4bf861e40026b5f2b977bfa7d7f4eaaf03ee196a
LeAnne B. <branchleannelmb@gmail.com>
2024-05-27 22:14:40 -0400

    console logs keypress

--- controls.js
+++ controls.js
@@ -16,7 +16,32 @@ class Controls {
           break;
         case "ArrowRight":
           this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
       }
+      console.table(this);
+    };
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+      console.table(this);
     };
   }
 }
